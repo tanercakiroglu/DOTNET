78da1ac654547dea424f08cdd94388ee0492ce01
taner çakıroğlu <tanercakiroglu@gmail.com>
2017-01-11 18:43:31 +0300

    unneccesary attribute removed

--- BuySellRent/CommonService.cs
+++ BuySellRent/CommonService.cs
@@ -7,7 +7,6 @@ using System.ServiceModel.Activation;
 namespace BuySellRent
 {
     [AutomapServiceBehavior]
-    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class CommonService : ICommonService
     {
         public List<Country> getAllCountries()
